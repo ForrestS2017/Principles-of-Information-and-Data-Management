454ca6608d0596f5f4da886ea133c3f4262c06f9
RBoppana <rahul32131@yahoo.com>
2018-11-16 16:12:47 -0500

    eskitit

---
+++ Project Files/bar-beer-drinker-ui/src/app/drinkers/drinkers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drinkers',
+  templateUrl: './drinkers.component.html',
+  styleUrls: ['./drinkers.component.css']
+})
+export class DrinkersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Project Files/bar-beer-drinker-ui/src/app/modifications/modifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modifications',
+  templateUrl: './modifications.component.html',
+  styleUrls: ['./modifications.component.css']
+})
+export class ModificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
