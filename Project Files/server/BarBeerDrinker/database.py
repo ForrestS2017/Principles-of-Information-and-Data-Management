4367f0e495137b4d7cddb3fce3d1f208c9fa9abe
Forrest Smith <fsmith2017@gmail.com>
2018-11-21 16:36:38 -0500

    Fixed Manf queries and page

--- Project Files/server/BarBeerDrinker/database.py
+++ Project Files/server/BarBeerDrinker/database.py
@@ -358,11 +358,21 @@ MANF PAGE
 
 def get_highest_sales(manf_name):
     with engine.connect() as con:
-        query = sql.text("select distinct b.Manf, bi.TipTotal, ba.City from Beers b, Bills bi, Bars ba where b.Manf = :manf_name and ba.BarName = bi.BarName and bi.ItemName in (select distinct b1.BeerName from Beers b1 where b1.Manf = :manf_name) order by bi.TipTotal desc")
+        query = sql.text("SELECT DISTINCT b.Manf, Cast(sum(bi.TipTotal) AS decimal(18,2)) AS TipTotal, ba.City \
+                        FROM Beers b, Bills bi, Bars ba \
+                        WHERE b.Manf = :manf_name AND  ba.BarName = bi.BarName AND bi.ItemName \
+                        IN (SELECT DISTINCT b1.BeerName from Beers b1 where b1.Manf = :manf_name) \
+                        GROUP BY ba.City\
+                        ORDER BY  bi.TipTotal DESC")
         rs = con.execute(query, manf_name=manf_name)
-        if rs.rowcount is 0:
-            return None
-        return [dict(row) for row in rs]
+        results = [dict(row) for row in rs]
+        for thisdict in results:            
+            target = list(thisdict.keys())
+            target = target[2]
+            thisdict[target] = int(thisdict[target])
+        for row in results:
+            print(row)
+        return results
 
 
 def get_liked_manfs(manf_name):
